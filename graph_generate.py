4e0e0fcaaf00498fd8555ba78699f0db77ebe643
aspenglow <aspenglow1998@gmail.com>
2023-05-23 00:56:05 +0200

    updated argparse.

--- graph_generate.py
+++ graph_generate.py
@@ -11,7 +11,8 @@ def parse_args():
     parse.add_argument('--save-dir-er', type=str, default='graphs/erdos_renyi', help='Save dir of ER graph.')
     parse.add_argument('--save-dir-sbm', type=str, default='graphs/sbm', help='Save dir of sbm graph.')
     parse.add_argument('--num-er', type=int, default=0, help='Number of ER graph.')
-    parse.add_argument('--num-sbm', type=int, default=1000, help='Number of SBM graph.')
+    parse.add_argument('--num-sbm', type=int, default=100, help='Number of SBM graph.')
+    parse.add_argument('--num-sbm-blocks', type=int, default=2, help='Number of blocks of SBM graph.')
     parse.add_argument('--index-start-at-er', type=int, default=1, help='Start index of ER graph.')
     parse.add_argument('--index-start-at-sbm', type=int, default=1, help='Start index of sbm graph.')
     args = parse.parse_args()
@@ -49,5 +50,5 @@ def generate_sbm_graph(save_dir='graphs/sbm', num_graphs=500, index_start_at=1, 
 if __name__ == "__main__":
     args = parse_args()
     generate_erdos_renyi_graph(save_dir=args.save_dir_er, num_graphs=args.num_er, index_start_at=args.index_start_at_er)
-    generate_sbm_graph(save_dir=args.save_dir_sbm, num_graphs=args.num_sbm, index_start_at=args.index_start_at_sbm)
+    generate_sbm_graph(save_dir=args.save_dir_sbm, num_graphs=args.num_sbm, index_start_at=args.index_start_at_sbm, num_blocks=args.num_sbm_blocks)
     
